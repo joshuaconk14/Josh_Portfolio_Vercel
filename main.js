8f9616c72bc7b3985dc95c493f14d65648060229
Joshua Conklin <joshuaconk14>
2024-11-22 00:36:48 -0800

    portfolio almost done

--- main.js
+++ main.js
@@ -1,11 +1,84 @@
-// - - - - - NAV BAR FUNCTION - - - - -
-
-function menuFunction() {
-    var menuButton = document.getElementById("myNavMenu") // variable for class w/ id "myNavMenu" from HTML (for drop down menu)
-
-    if(menuButton.className === "nav-menu"){ // if className is nav-menu
-        menuButton.className += "responsive";
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu"); // gets nav menu element through its id and places in const
+    if(menuBtn.className === "nav-menu"){ // if className is nav-menu
+      menuBtn.className += " responsive"; // space MUST be included when adding this to className
     } else {
-        menuButton.className = "nav-menu";
+      menuBtn.className = "nav-menu";
     }
+  }
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()}; // when user scrolls, this will happen; navShadow() = event
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+    
+    // if scroll position of body element || doc element is greater than 50 pixels
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+    } else {
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+    }
+  }
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+/* -- HOME -- */
+sr.reveal('.hello',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-description',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+sr.reveal('.project-text-box',{delay: 200})
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+  const scrollY = window.scrollY;
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }  else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
 }
+window.addEventListener('scroll', scrollActive)
